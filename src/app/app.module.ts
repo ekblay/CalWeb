911d82cd169b677761545eaf28b556424c2ffd57
ekblay <43041985+ekblay@users.noreply.github.com>
2020-08-25 12:29:22 -0300

    migrating into angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppHomeComponent } from './home/app.home.component';
+
+
+// Navigation bar and footer have to be in the module so
+// that it is accessible to all templates and components
+
+// Navigation
+@Component({
+  selector: 'app-nav-template',
+  templateUrl: './templates/navBar.html'
+})
+
+export  class NavigationComponent {
+  logoImage = './assets/img/logo.jpg';
+}
+
+
+// Footer
+@Component({
+  selector: 'app-footer-template',
+  templateUrl: './templates/footer.html'
+})
+
+export class FooterComponent {}
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppHomeComponent, NavigationComponent, FooterComponent],
   imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppHomeComponent],
 })
 export class AppModule {}
---
+++ src/app/home/app.home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.home.component.html'
+})
+export class AppHomeComponent {
+   // title = 'angular';
+   images = ['./assets/img/church.jpg', './assets/img/flags1.jpg', './assets/img/pastor1.jpg'];
+}
+
+
